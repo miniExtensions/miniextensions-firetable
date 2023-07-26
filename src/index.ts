ea872b1fea2f7aa84e7d1949eb2d88477a95a526
Abdul Alzenki <bojamal7@gmail.com>
2023-07-26 12:01:08 -0700

    Add firetableUsersFieldName

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import {
     updateRecord,
     deleteRecord,
 } from './airtable';
+import { firetableUsersFieldName } from './firetableUsersFieldName';
 
 const initializeApp = (args: { config: FiretableConfig }): void => {
     setState({
@@ -35,4 +36,5 @@ export {
     createRecord,
     updateRecord,
     deleteRecord,
+    firetableUsersFieldName,
 };
